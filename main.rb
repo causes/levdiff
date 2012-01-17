cbea96f5dfa1fc465fa7cffce21ebffa0577926a
Adam Derewecki <adam@causes.com>
2012-01-16 21:39:05 -0800

    take THRESHOLD environment variable

--- main.rb
+++ main.rb
@@ -2,13 +2,16 @@
 
 require 'text'
 
+# threshold Levenshtein edit distance to suppress output
+# will only output the lines if distance < threshold
+threshold = ENV['THRESHOLD'].to_i || 10
+
 STDIN.readlines.each_slice(2) do |a, b|
   pairs = [[@last || '', a], [a, b]]
 
   pairs.each do |x, y|
     distance = Text::Levenshtein.distance(x, y)
-    puts distance
-    puts [x, y].join if distance < 10
+    puts [x, y].join if distance < threshold
     @last = b
   end
 end
